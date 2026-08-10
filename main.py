c4910e835232d3a97566354cd7cabcfa1082b490
thatsnotpcbro <rwitkowski6@gmail.com>
2026-08-10 17:50:06 -0400

    H: Added Shot function when SPACEBAR is pressed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 import pygame
 from player import Player
+from shot import Shot
 
 def main():
     pygame.init()
@@ -17,9 +18,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
